76c01c03b29b9f7cd8f9e52903a8f7a2e893dd10
Zeinok <zeinok@protonmail.com>
2020-10-23 23:00:40 +0800

    Add Invidious redirect script

---
+++ invidious-redirect.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        Invidious Redirect
+// @description Redirect YouTube video link to Invidious instances
+// @namespace   io.github.zeinok.invidiousRedirect
+// @match       *://youtube.com/watch
+// @match       *://*.youtube.com/watch
+// @match       *://youtu.be/
+// @grant       none
+// @version     1.0
+// @author      Zeinok
+// ==/UserScript==
+
+/* CONFIG */
+// List of invidious instances, will be randomly chosen upon redirection.
+// URI schema (https://) is required.
+// View list of instances here: https://instances.invidio.us
+const instances = ["https://invidious.snopyta.org", "https://yewtu.be", "https://invidious.xyz", "https://yewtu.be/"];
+
+/* SCRIPT START */
+let url = new URL(window.location);
+let redirectURL = new URL(instances[Math.floor(Math.random()*instances.length)])
+let pathname = url.pathname.substring(1);
+let videoID = url.searchParams.get("v");
+let time = url.searchParams.get("t");
+
+redirectURL.pathname = "/watch";
+if(!videoID)
+  videoID = pathname;
+redirectURL.searchParams.set("v", videoID);
+if(time)
+  redirectURL.searchParams.set("t", time);
+
+window.location = redirectURL.toString();
